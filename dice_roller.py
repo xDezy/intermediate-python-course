8b10fc57d0cefe1af0df4ba3768cfccb7a8f3526
xDezy <60003449+xDezy@users.noreply.github.com>
2020-09-18 00:14:39 +0200

    yes

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
+import random
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You have rolled a {roll}')
+print(f"Your total is {dice_sum}")
+
 def main():
-  print('You rolled a die')
+  print('')
 
 if __name__== "__main__":
   main()
